880a09ae1a9c996f00c5319e04062a2e8f3caa99
Mainul <mainulhasan@iut-dhaka.edu>
2025-03-09 15:46:07 +0600

    refactor: comment out cookie setting in login response

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -40,13 +40,13 @@ const loginUser = asyncHandler(async (req, res) => {
         const token = user.getJwtToken();
     
         
-        // Set token in HTTP-only cookie
-        res.cookie("token", token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "None",
-            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-        });
+        // // Set token in HTTP-only cookie
+        // res.cookie("token", token, {
+        //     httpOnly: true,
+        //     secure: process.env.NODE_ENV === "production",
+        //     sameSite: "None",
+        //     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        // });
         console.log("Sending response:", JSON.stringify({ user, token }, null, 2));
         return new ApiResponse(200, { user, token }, "Login successful").send(res);
         
